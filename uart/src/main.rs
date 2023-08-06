44c33bc7c48f226ff7e928dfed470da78789d967
Jack (Butch) Griffin <butchg@comcast.net>
2023-08-06 06:18:38 -0700

    update to clean up device generation

--- uart/src/main.rs
+++ uart/src/main.rs
@@ -1,9 +1,20 @@
 #![no_std]
 #![no_main]
+#[allow(unused_imports)]
+
+use panic_halt as _;
+use cortex_m::asm;
+use cortex_m_rt::* ;
+use psoc6dev::psoc602::CPUSS::Cpuss ;
 
 #[entry]
 fn main() -> ! {
-    loop {
-        asm::nop() ;
+    unsafe {
+        let p = 0x0900 as *mut ();
+        let _c = Cpuss::from_ptr(p);
+
+        loop {
+            asm::nop() ;
+        }
     }
 }
